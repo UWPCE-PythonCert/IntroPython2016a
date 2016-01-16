258d6654f6143efb6b920e314fb5579a44dbb896
Bryan Glogowski <bglogowski@dhcp-34.pdx>
2016-01-16 14:29:57 -0800

    created scripts for week 2 homework

---
+++ students/bryan_glogowski/session02/fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+n = int(input('Please enter a number: '))
+
+if n > 0:
+    for i in range(1,n+1):
+        s = ''
+        if i % 3 == 0:
+            s += 'Fizz'
+        if i % 5 == 0:
+            s += 'Buzz'
+        if len(s) > 0:
+            print(s)
+else:
+    print('Sorry, but "{}" is not a natural number!'.format(n))
+
