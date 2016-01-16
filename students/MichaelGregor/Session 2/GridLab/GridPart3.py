829f4466e2ad930c767cd34c0e3a6e14082fa51e
Michael Gregor <michaelgregor2600@gmail.com>
2016-01-16 15:53:13 -0800

    Part 3 of Grid lab is complete, still working on part 2

---
+++ students/MichaelGregor/Session 2/GridLab/GridPart3.py
@@ -0,0 +1,65 @@
+
+def main():
+    print_grid(3,8)
+
+def print_grid(dimensions=2, spaces=3):
+    ''' This function builds the grid based on the dimensions and size of the box provided
+
+    :param dimensions: The value squared to give the full dimensions of the grid (default=2)
+    :type dimensions: int
+    :param spaces: The number of spaces between the walls of each box(default=3)
+    :type spaces: int
+    :return: None
+    '''
+
+    for x in range(dimensions):
+        print_line(dimensions, spaces, isLine=True)
+        for x in range(spaces):
+            print_line(dimensions, spaces, isLine=False)
+    print_line(dimensions, spaces, isLine=True)
+
+def print_line(num_plus, num_dash, isLine):
+    '''
+    This function prints a line in the grid.  A line with + and - characters is isLine is True.  A line with
+    | and space characters if it is false.  We keep this in one function and paramaterize the function to aid
+    in code reuse.
+
+    :param num_plus: The number of + characters or columns we want in the grid
+    :type num_plus: int
+    :param num_dash: The number of - or spaces horizontally for each box
+    :type num_dash: int
+    :param isLine: If True, we will print a line that has + and - characters.  If False, we do | and space characters
+    :type: bool
+    :return: None
+    '''
+    # Checking to see if we are printing a line with +'s or a line with pipes
+    if isLine:
+        firstChar = "+"
+        secondChar = "-"
+    else:
+        firstChar = "|"
+        secondChar = " "
+
+    plus_counter = 0
+    space_counter = 0
+
+    # We begin looping based on the dimensions provided
+
+    # First while loop is for the + or | character printing
+    while plus_counter < num_plus:
+        print(firstChar, end="")
+
+        # Second while loop is for the - or space characters
+        while space_counter < num_dash:
+            print(secondChar, end="")
+            space_counter += 1
+
+        # We have to re-initialize the space counter or the loop with exit immediately after we print the next
+        # + or | character
+        space_counter=0
+
+        plus_counter +=1
+    print(firstChar)
+
+if __name__ == "__main__":
+    main()
