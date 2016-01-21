5aef44aba87118904bbac49b77f9fa9c090e29b5
Mike Schincariol <mikeschincariol@gmail.com>
2016-01-20 22:10:45 -0500

    Optiization: I forgot about the mod (%) function.

--- students/MikeSchincariol/session2/FizzBuzz/FizzBuzz.py
+++ students/MikeSchincariol/session2/FizzBuzz/FizzBuzz.py
@@ -3,11 +3,11 @@ def FizzBuzz(n):
     if not isinstance(n, int):
             raise TypeError("FIZZBUZZ: Argument of type {0} is not supported. Only type 'int' is allowed".format(type(n)))
     for i in range(1, n+1):
-        if (i / 3 == i // 3 and i / 5 == i // 5):
+        if (i % 3 == 0 and i % 5 == 0):
             print('FizzBuzz')
-        elif (i / 3 == i // 3):
+        elif (i % 3 == 0):
             print('Fizz')
-        elif (i / 5 == i // 5):
+        elif (i % 5 == 0):
             print('Buzz')
         else:
             print('{0}'.format(i))
