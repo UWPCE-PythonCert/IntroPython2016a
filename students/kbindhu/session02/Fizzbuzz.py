002ef841c742a896a69058308520c731b2445485
krishna Bindhu <krishna.sidharthan@gmail.com>
2016-01-21 14:09:46 -0800

    Session02 lab submission

---
+++ students/kbindhu/session02/Fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+for i in range(1,101):
+    if i%3==0:
+        if i%5==0:
+            print ("FizzBuzz")
+        else:
+            print("Fizz")
+    elif (i%5==0):
+        print("Buzz")
+    else:
+        print(i)
