657241a469e37fd6f3c81da6acf198a811e500b9
Greg McKeag <gmckeag@cogentloci.com>
2016-01-22 13:24:22 -0800

    tweaked list lab

--- students/gmckeag/session3/list_lab.py
+++ students/gmckeag/session3/list_lab.py
@@ -3,26 +3,6 @@
 # list_lab.py
 #
 
-"""
-When the script is run, it should accomplish the following four series of actions:
-
-Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
-Display the list.
-Ask the user for another fruit and add it to the end of the list.
-Display the list.
-Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis).
-Add another fruit to the beginning of the list using “+” and display the list.
-Add another fruit to the beginning of the list using insert() and display the list.
-Display all the fruits that begin with “P”, using a for loop.
-Using the list created in series 1 above:
-
-Display the list.
-Remove the last fruit from the list.
-Display the list.
-Ask the user for a fruit to delete and find it and delete it.
-(Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
-"""
-
 if __name__ == '__main__':
 
     # Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
