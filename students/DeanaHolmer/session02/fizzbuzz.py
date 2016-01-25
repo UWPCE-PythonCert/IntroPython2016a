6795a1697c4189c20ea51557f0b1a80e8634b31f
Summer Rae <summerela@users.noreply.github.com>
2016-01-25 13:31:24 -0800

    Nicely done!

--- students/DeanaHolmer/session02/fizzbuzz.py
+++ students/DeanaHolmer/session02/fizzbuzz.py
@@ -9,10 +9,11 @@
 import os
 os.chdir('C:\\Documents\\Python\\python35src\\UWPython1\\week2')
 
+# do you know how to ask the user for input instead of hard-coding these variables? 
 fizznum = 3
 buzznum = 5
 for i in range(1, 101):
-    if i % (fizznum * buzznum) == 0:
+    if i % (fizznum * buzznum) == 0: # could you also use an 'OR' here?  
         print("fizzbuzz")
     elif i % fizznum == 0:
         print("fizz")
