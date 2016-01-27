fbe85966910ae014d598497a7226223dd4563612
Michael Gregor <michaelgregor2600@gmail.com>
2016-01-26 20:57:28 -0800

    Completed simple version to provide me a framework to work with.  Need to complete more complex version to preserve casing, punctuation, and spaces.

--- students/MichaelGregor/Sesson 3/rot13.py
+++ students/MichaelGregor/Sesson 3/rot13.py
@@ -1,17 +1,57 @@
+'''
+def encode(phrase):
 
+    phrase_to_encode = phrase
+    enconded_phrase = ""
+
+    for char in phrase_to_encode:
+
+        newChar = ord(char)
+        newChar = newChar + 13
+        enconded_phrase = enconded_phrase + chr(newChar)
+
+
+    print(enconded_phrase)
+
+def decode(phrase):
+
+    phrase_to_decode = phrase
+    decoded_phrase = ""
+
+    for char in phrase_to_decode:
+
+        newChar = ord(char)
+        newChar = newChar - 13
+        decoded_phrase = decoded_phrase + chr(newChar)
+
+    print(decoded_phrase)
 
 def main():
 
-    phrase_to_encode = "Michael"
+    text_to_alter = input("Enter some text you wish to alter: ")
 
-    for char in phrase_to_encode:
-        if char == ' ':
-            continue
+    print("Enter 1 to encode\nEnter 2 to decode")
+    execution = input(":")
 
-        phrase_to_encode.replace(char, (chr((ord(char) + 13))))
-        print(char)
+    while not (execution == "1" or execution == "2"):
+        execution = input("Please enter 1 or 2 only:")
 
-    print(phrase_to_encode)
+    if execution == "1":
+        encode(text_to_alter)
+    elif execution == "2":
+        decode(text_to_alter)
+
+'''
+
+def main():
+
+
+def rot13(phrase):
+
+    encoded_phrase = ""
+    for character in phrase():
+        if character.isalpha() and character.isupper():
+            
 
 
 if __name__ == '__main__':
