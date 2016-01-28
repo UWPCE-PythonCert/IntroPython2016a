cc46de7bca615ef7c2219111612eeda9df8bc5b2
ThomasGaffneyIV@Gmail.com <thomasgaffneyiv@gmail.com>
2016-01-27 23:09:21 -0600

    Updated Fizzbuzz with Summer's Comments

--- students/Thomas_Gaffney/session_02/FizzBuzz.py
+++ students/Thomas_Gaffney/session_02/FizzBuzz.py
@@ -1,8 +1,8 @@
 def fizzBuzz():
     for i in range(1,101):
-    	if((i%3 == 0) and (i%5 !=0)):  print("Fizz")
-    	elif((i%3 != 0) and (i%5 ==0)):  print("Buzz")
-    	elif((i%3 == 0) and (i%5 ==0)):  print("FizzBuzz")
-    	else: print(i)
+        if((i%3 == 0) and (i%5 ==0)): print("FizzBuzz")
+        elif(i%3 == 0):  print("Fizz")
+        elif(i%5 == 0):  print("Buzz")
+        else: print(i)
 
 fizzBuzz()
