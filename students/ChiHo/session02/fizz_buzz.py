cec0bcea84fc1711a50120b6133d5639b305d73c
chiho924 <chiho924@msn.com>
2016-01-28 16:21:47 -0500

    Revised my fizz_buzz.py program to make it more efficient.

--- students/ChiHo/session02/fizz_buzz.py
+++ students/ChiHo/session02/fizz_buzz.py
@@ -1,5 +1,5 @@
 # Week 2 - Lab #1: FizzBuzz
-# Date: Saturday, January 16, 2016
+# Date: Thursday, January 28, 2016
 # Student: Chi Kin Ho
 
 
@@ -12,28 +12,26 @@ def fizz_buzz(n):
        Numbers divisible by both factors should display "FizzBuzz".
     """
 
-    message = ''
     for i in range(1, n+1):
-        is_divisible = False
-        if i % 3 == 0: # divisible by 3
-            message += 'Fizz\n'
-            is_divisible = True
-        if i % 5 == 0: # divisible by 5
-            message += 'Buzz\n'
-            is_divisible = True
-        if not is_divisible: # not divisible by 3 and/or 5
-            message += str(i) + '\n'
-    # Display the new message on the screen.
-    print(message)
 
+        if i % 3 == 0:
+            if i % 5 == 0: # divisible by 3 and 5
+                print('FizzBuzz')
+            else: # divisible by 3 only
+                print('Fizz')
+        elif i % 5 == 0: # divisible by 5 only
+            print('Buzz')
+        else: # neither divisible by 3 nor divisible by 5
+            print(i)
 
-# Test Case 1: n = 1
-fizz_buzz(1)
-# Test Case 2: n = 3
-fizz_buzz(3)
-# Test case 3: n = 5
-fizz_buzz(5)
-# Test case 4: n = 10
-fizz_buzz(10)
-# Test case 5: n = 16
-fizz_buzz(16)
+if __name__ == '__main__':
+    # Test Case 1: n = 1
+    fizz_buzz(1)
+    # Test Case 2: n = 3
+    fizz_buzz(3)
+    # Test case 3: n = 5
+    fizz_buzz(5)
+    # Test case 4: n = 10
+    fizz_buzz(10)
+    # Test case 5: n = 16
+    fizz_buzz(16)
