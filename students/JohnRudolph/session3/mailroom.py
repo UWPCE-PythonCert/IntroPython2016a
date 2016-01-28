831db99c2ef8baea4ea7cebf6e6b9f5e84392cfd
John Rudolph <john.rudolph08@gmail.com>
2016-01-28 00:54:25 -0500

    updatede through 'thank you' mailroom

--- students/JohnRudolph/session3/mailroom.py
+++ students/JohnRudolph/session3/mailroom.py
@@ -1,6 +1,15 @@
-#Creating list for each donor containing donation amounts
-#onorlist = [('donor1', 100, 200, 300)]
-#donorlist.append(('donor2', 200, 300, 400))
+'''
+################################################################
+				CREATE DONOR LIST AND ASK FOR ACTION
+################################################################
+It should have a data structure that holds a list of your donors 
+and a history of the amounts they have donated. This structure 
+should be populated at first with at least five donors, with between 
+1 and 3 donations each
+
+The script should prompt the user (you) to choose from a menu of 
+2 actions: ‘Send a Thank You’ or ‘Create a Report’
+'''
 
 import random
 
@@ -27,6 +36,7 @@ def createDonors(numDonors):
 
 	return donorList
 
+#create an arbitrary # of donors - 5 per assignment directions
 numDonors = 5
 # dreate the initial list of donors
 donorList = createDonors(numDonors)
@@ -48,7 +58,60 @@ while index_check is None:
 	else:
 		index_check = True
 
-print(getAction)
+'''
+################################################################################
+						THANK YOU ACTION
+################################################################################
+If the user (you) selects ‘Send a Thank You’, prompt for a Full Name.
+If the user types ‘list’, show them a list of the donor names and re-prompt
+If the user types a name not in the list, add that name to the data 
+    structure and use it.
+If the user types a name in the list, use it.
+Once a name has been selected, prompt for a donation amount.
+Verify that the amount is in fact a number, and re-prompt if it isn’t.
+Once an amount has been given, add that amount to the donation history 
+    of the selected user.
+Finally, use string formatting to compose an email thanking the donor for 
+    their generous donation. Print the email to the terminal and return to 
+    the original prompt.
+'''
+
+def appendDonor(getName):
+	'''
+	This function find the donor in the dictionary
+	Prompts user for new donations and appends donation to the dictionary key
+	If user does not enter a number then error will be raised
+	'''
+	donCheck = None
+	while donCheck is None:
+		try:
+			donation = float(input('Enter donation amount:\n'))
+			ownerDic.setdefault(getName, []).append(donation)
+			print(ownerDic[getName])
+			donCheck = True
+		except:
+			print('You did not enter a number. Try Again!')
+
+if getAction == 'send a thank you':
+	getName = input("Input a full name\n")
+	if getName.lower() == 'list':
+		print(ownerDic)
+	elif getName in ownerDic.keys():		
+		appendDonor(getName)
+	else:
+		print('Adding ', getName, ' to Donor Database')
+		ownerDic.setdefault(getName, [])
+		appendDonor(getName)
+
+
+
+
+
+
+
+
+
+
 		
 
 
