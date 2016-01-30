fbe4c6aa560e5f0a0f92818d6a1f3da54c8b3e6a
barlaensdoonn <kestrel@KESTRELs-MacBook-Pro.local>
2016-01-29 22:12:04 -0800

    made some progress with the initial prompt, it now passes a variable to a redirect function to execute other functions or quit

--- students/brandon_aleson/session04/mailroom.py
+++ students/brandon_aleson/session04/mailroom.py
@@ -16,6 +16,40 @@ def initializeDict():
     return donors
 
 
+def choose():
+    action = input('(type \'s\' to send a thank you message, \'c\' to create a report, or \'q\' to quit)\n')
+    while (action != 's' and action != 'c' and action != 'q'):
+        action = input('I\'m not doing anything until you give me one of those three letters\n')
+    if action == 's':
+        return(action)
+    elif action == 'c':
+        return(action)
+    elif action == 'q':
+        return False
+
+
+def sendThanks():
+    print('sending thanks')
+
+
+def createReport():
+    print('creating report')
+
+
+def redirect(choice):
+    if not choice:
+        print('I\'ll be here when you need me')
+    elif choice == 's':
+        sendThanks()
+    elif choice == 'c':
+        createReport()
+
+
 if __name__ == '__main__':
     initializeDict()
-    print(donors)
+
+    print('Welcome to the mailroom!')
+    print('What would you like to do?')
+
+    choice = choose()
+    redirect(choice)
