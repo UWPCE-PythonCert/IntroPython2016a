acb5df4d6d7e812394f431d1d423ec4eecb42a92
Michael Gregor <michaelgregor2600@gmail.com>
2016-01-30 15:53:43 -0800

    Fixed dictionary structure and made dict global for editing ease. Added the following functions: get_donor_donations()

--- students/MichaelGregor/Session 4/mailroom.py
+++ students/MichaelGregor/Session 4/mailroom.py
@@ -2,6 +2,11 @@ import os
 import platform
 
 
+donor_list = {'Mike Singletary':[50.00, 100.00, 200.00],
+                  'Mike Ditka':[300.00, 500.00, 50.00],
+                  'Sweetness': [20.00, 700.00],
+                  'The Fridge': [900.00, 2000.00],
+                  'Dan Hampton': [3000.00]}
 
 def show_main_menu():
 
@@ -27,15 +32,46 @@ def show_main_menu():
         print("**************************************************")
         menu_option = input(":")
 
-    return menu_option
+    if menu_option == '1':
+        send_thank_you()
+    elif menu_option == '2':
+        create_report()
+    else:
+        print("Thank you for using The Mailroom!")
 
 def send_thank_you():
 
-    names = get_donor_names()
+
+    full_name = input("Please provide the full name: ")
+
+    if full_name not in get_donor_names():
+
+        create_new_name = ''
+
+        while not (create_new_name == '1' or create_new_name == '2'):
+
+            print(("{} is not in the current list of donors\n Do you wish to create a new donor?".format(full_name)))
+            print("1. Yes")
+            print("2. No")
+            create_new_name = input(":")
+
+        if create_new_name == '1':
+            create_new_donor(full_name)
+        else:
+            show_main_menu()
+
+    else:
+        show_donor_list()
+
+def create_new_donor(new_donor):
+
+    donations = []
 
 
 
+    donor_list.update({new_donor: donations})
 
+    print(new_donor)
 
 def create_report():
 
@@ -52,42 +88,46 @@ def clear_screen():
         os.system('clear')
 
 def get_donor_names():
-    option = ''
+
     donors = get_donor_list()
     names = []
 
-    for values in donors:
-        donor_name = donors[values]['full_name']
-        names.append(donor_name)
+    for name in donors:
+        names.append(name)
 
+    return names
 
+def get_donor_donations(donar_name):
+
+    donors = get_donor_list()
+
+    for name in donors:
+        if name == donar_name:
+            return donors[name]
 
 def get_donor_list():
-    donor_list = {'donor1':{'full_name':'Mike Singletary', 'donations': [50.00, 100.00, 200.00]},
-                  'donor2':{'full_name':'Mike Ditka', 'donations': [300.00, 500.00, 50.00]},
-                  'donor3':{'full_name':'Sweetness', 'donations': [20.00, 700.00]},
-                  'donor4':{'full_name':'The Fridge', 'donations': [900.00, 2000.00]},
-                  'donor5':{'full_name':'Dan Hampton', 'donations': [3000.00]}}
 
+    global donor_list
     return donor_list
 
 def show_donor_list():
 
-    donor_list = get_donor_list()
+    clear_screen()
+
+    
+
+    print("**************************************************")
+    print("*                                                *")
+    print("*    {}              ${.2f}")
 
     print(donor_list.values())
 
 
 def main():
 
-    menu_option = show_main_menu()
+    show_main_menu()
 
-    if menu_option == '1':
-        send_thank_you()
-    elif menu_option == '2':
-        create_report()
-    else:
-        print("Thank you for using the Mailroom.")
+
 
 
 
