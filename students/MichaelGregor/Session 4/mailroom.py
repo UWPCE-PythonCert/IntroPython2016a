321136133090a30386474cc37e1d51f21367a356
Michael Gregor <michaelgregor2600@gmail.com>
2016-01-31 13:45:38 -0800

    Final comments and docs created.  All done.

--- students/MichaelGregor/Session 4/mailroom.py
+++ students/MichaelGregor/Session 4/mailroom.py
@@ -1,8 +1,8 @@
 import os
 import platform
-import time
 from operator import itemgetter
 
+# Create global dictionaries so that multiple methods can used them for data flow control
 
 donor_list = {'Mike Singletary':[50.00, 100.00, 200.00],
                   'Mike Ditka':[300.00, 500.00, 50.00],
@@ -14,6 +14,11 @@ donor_final_report_list = {}
 
 
 def show_main_menu():
+    """
+    Shows the main menu and is the beginning of the control flow
+
+    :return: None
+    """
 
     menu_option = ''
 
@@ -45,6 +50,11 @@ def show_main_menu():
 
 
 def send_thank_you():
+    """
+    Launches the Send Thank You control flow. Use to list the current donors available or create a new donor.
+
+    :return:  None
+    """
 
     full_name = input("Please provide the full name: ")
 
@@ -72,6 +82,13 @@ def send_thank_you():
 
 
 def donor_management(full_name):
+    """
+    This function is used to Add donations to a specific donor and to trigger the final thank you email.
+
+    :param full_name: The name of the donor who we are adding a donation to or sending email
+    :type full_name: String
+    :return: None
+    """
 
     option = ''
 
@@ -98,6 +115,11 @@ def donor_management(full_name):
 
 
 def create_report():
+    """
+    This prints a report of all the donors, their total donation amount, number of donations, and average
+
+    :return:
+    """
 
     clear_screen()
 
@@ -105,29 +127,36 @@ def create_report():
 
     donor_totals_list = {}
 
-    for name in donors:
-        donor_report_list = []
+    for name in donors:                                                 # We need to get each donor and start to collect
+        donor_report_list = []                                          # data to build the report.
 
         total_donations = 0.0
         num_of_donations = float(len(get_donor_donations_data(name)))
         donations = get_donor_donations_data(name)
 
-        for donation in donations:
-            total_donations += donation
-
+        for donation in donations:                                      # All the donor donations are in List format so
+            total_donations += donation                                 # so we have to iterate through each donation
+                                                                        # and total them together.
         average_donation = total_donations / num_of_donations
-        donor_report_list.append(total_donations)
-        donor_report_list.append(average_donation)
+        donor_report_list.append(total_donations)                       # Here we begin to build a new Dictionary object
+        donor_report_list.append(average_donation)                      # to hold the data we need for the final report.
         donor_report_list.append(int(num_of_donations))
 
-        donor_totals_list.update({name: total_donations})
-
-        donor_final_report_list.update({name: donor_report_list})
+        donor_totals_list.update({name: total_donations})               # We create a new totals dictionary object so
+                                                                        # so we can use it later to sort the report by
+        donor_final_report_list.update({name: donor_report_list})       # total amount of donations.
 
     print("*****************************************************************")
     print("*                            DONOR REPORT                       *")
     print("*                                                               *")
     print("*  NAME                   TOTAL    NUMBER     AVERAGE           *")
+
+
+    # Woot, my only stack overflow answer in this program.  So we are sorting each name in the dictionary by the
+    # donor_totals_list.  This will by default sort smallest to largest.  The 'reverse=True' statement allows the
+    # us to sort the names by the donation total largest to smallest.  We then iterate through each name, gather a
+    # the donor report which is a List item pulled from the global donor_final_report_list and format the output in
+    # the print statement.
 
     for name,value in sorted(donor_totals_list.items(), key=itemgetter(1), reverse=True):
         donor_report = get_donor_donations_data(name, final=True)
@@ -141,16 +170,23 @@ def create_report():
 
 
 def add_donor_donation(full_name):
+    """
+    This adds a donation amount to a specific donor.
+
+    :param full_name: Name of the donor that we are adding a donation too.
+    :type full_name: String
+    :return: Donor name (full_name)
+    """
 
     donations = []
     print("Please enter a donation amount for {}".format(full_name))
     donation_amount = ''
 
-    while not donation_amount.isdigit():
-        donation_amount = input("No decimals please:$")
-
-    donation_amount = float(donation_amount)
-    donations.append(donation_amount)
+    while not donation_amount.isdigit():                                # Check to make sure the donation amount is a
+        donation_amount = input("No decimals please:$")                 # a integer number as a string to prevent
+                                                                        # weird data being entered.
+    donation_amount = float(donation_amount)                            # We then cast it as a float before we add it
+    donations.append(donation_amount)                                   # to the donor's donations for easy math later.
     donor_list.update({full_name: donations})
 
     print("A ${:.2f} donation has been added to {}'s donation history".format(donation_amount, full_name))
@@ -160,6 +196,13 @@ def add_donor_donation(full_name):
 
 
 def send_email(full_name):
+    """
+    Formats an email and prints it to the screen.
+
+    :param full_name: Donor Name
+    :type full_name: String
+    :return: Donor Name (full_name)
+    """
 
     donations = get_donor_donations_data(full_name)
     total_donations = 0.0
@@ -177,6 +220,12 @@ def send_email(full_name):
 
 
 def clear_screen():
+    """
+    Determines what operating system is currently running and executes the applicable command line argument to clear
+    the screen.
+
+    :return: None
+    """
 
     operating_systems = platform.system()
 
@@ -187,6 +236,11 @@ def clear_screen():
 
 
 def get_donor_names():
+    """
+    Iterates through our donor list and returns the names (i.e. the Keys for the donor dictionary objects)
+
+    :return: Donor names as a List object (names)
+    """
 
     donors = get_donor_list()
     names = []
@@ -198,6 +252,16 @@ def get_donor_names():
 
 
 def get_donor_donations_data(donar_name, final=False):
+    """
+    Gets the donation data for a particular donor.
+
+    :param donar_name: Donor name
+    :type donar_name: String
+    :param final: Checks to see if we are getting data for the final report
+    :type final: Bool
+
+    :return: Returns the List Object inside the applicable donor dictionary (donor[name])
+    """
 
     if final == True:
         donors = get_final_report_list()
@@ -210,18 +274,34 @@ def get_donor_donations_data(donar_name, final=False):
 
 
 def get_donor_list():
+    """
+    Gets the global donor list
+
+    :return: Dictionary Object of Donors and their donations (donor_list)
+    """
 
     global donor_list
     return donor_list
 
 
 def get_final_report_list():
+    """
+    Gets the global donor final report
+
+    :return:  Dictionary Object of Donors and their total donation amount, number of donations, and average
+              donation (donor_final_report_list)
+    """
 
     global donor_final_report_list
     return donor_final_report_list
 
 
 def show_donor_list():
+    """
+    Show the list of donor names
+
+    :return: None
+    """
 
     clear_screen()
 
@@ -240,6 +320,11 @@ def show_donor_list():
 
 
 def main():
+    """
+    Launches Program
+
+    :return: None
+    """
 
     show_main_menu()
 
