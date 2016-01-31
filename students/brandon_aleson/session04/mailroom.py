5aa8290a1c45d9644d68e5bc6da63a4c8b60d4fe
barlaensdoonn <kestrel@KESTRELs-MacBook-Pro.local>
2016-01-30 23:55:25 -0800

    added comments for the code, and added print()'s and input()'s to try to make the script's output more legible

--- students/brandon_aleson/session04/mailroom.py
+++ students/brandon_aleson/session04/mailroom.py
@@ -16,6 +16,7 @@ def initializeDict():
     return donors
 
 
+# control flow handler
 def choose():
     print('What would you like to do?')
     action = input('(type \'s\' to send a thank you message, \'c\' to create a report, or \'q\' to quit)\n')
@@ -31,6 +32,36 @@ def choose():
         return False
 
 
+# main function for sending thank you email
+def sendThanks():
+    input('Let\'s send some thanks!\n')
+    name, amount = donorInput()
+    sendEmail(name, amount)
+    input()
+
+
+# receive input on which donor to thank
+# if donor doesn't exist, add to the dictionary
+def donorInput():
+    name = input('Give me a full name, or type \'l\' to list the current donors on file\n')
+    while name == 'l':
+        printDonors()
+        name = input('Choose one of the above, or give me a new name\n')
+    donors.setdefault(name, [])
+    while True:
+        try:
+            amount = input('How much did they give?\n')
+            amount = int(amount)
+            print()
+            break
+        except ValueError:
+            print('That\'s not nice, you almost broke me')
+            print('Donations are usually positive integers')
+    donors[name].append(amount)
+    return (name, amount)
+
+
+# print current list of donors
 def printDonors():
     print('Here are the current donors:')
     for d in donors:
@@ -38,39 +69,19 @@ def printDonors():
     return ""
 
 
-def donorInput():
-    name = input('Give me a full name, or type \'l\' to list the current donors on file\n')
-    while name == 'l':
-        name = input(printDonors())
-    donors.setdefault(name, [])
-    while True:
-        try:
-            amount = input('How much did they give?\n')
-            amount = int(amount)
-            break
-        except ValueError:
-            print('That\'s not nice, you almost broke me\nDonations are usually positive integers')
-    donors[name].append(amount)
-    return (name, amount)
-
-
+# send formatted thank you email
 def sendEmail(name, amount):
     print('What a nice person!')
-    print('Let\'s send them this heartfelt email:')
+    input('Let\'s send them this heartfelt email:\n')
     print('Dear {},\nThank you for your donation of ${:,}.\nYou\'re great!'.format(name, amount))
 
 
-def sendThanks():
-    print('Let\'s send some thanks!')
-    name, amount = donorInput()
-    sendEmail(name, amount)
-    input()
-
-
+# sorting key function for return value of computeSortedStats()
 def statSorter(sorter):
     return sorter[3]
 
 
+# compile relevant donor statistcs into a sorted list for printReport()
 def computeSortedStats():
     stats = []
     for d, a in donors.items():
@@ -83,17 +94,18 @@ def computeSortedStats():
     return sorted(stats, key=statSorter)
 
 
+# print formatted donor report
 def printReport(statList):
     print('{:-^80}'.format('DONOR REPORT'))
     print('{:<20}{:<20}{:<20}{:<20}'.format('DONOR NAME', '# OF DONATIONS', 'AVERAGE AMOUNT', 'TOTAL DONATED'))
     for item in statList:
-        print('{:<20}{:<20}{:<20}{:,}'.format(item[0], item[1], item[2], item[3]))
+        print('{:<20}{:<20}${:<20}${:,}'.format(item[0], item[1], item[2], item[3]))
     print('{:-^80}'.format('--'))
 
 
+# main function for creating donor report
 def createReport():
-    print('crunching the numbers...')
-    input()
+    input('crunching the numbers...\n')
     statList = computeSortedStats()
     printReport(statList)
     input()
@@ -103,7 +115,7 @@ if __name__ == '__main__':
     initializeDict()
     loop = True
 
-    print('Welcome to the mailroom!')
+    input('Welcome to the mailroom!\n')
 
     while loop:
         loop = choose()
