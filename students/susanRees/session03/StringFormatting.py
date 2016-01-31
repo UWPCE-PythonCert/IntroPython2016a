574a59f907f43278e633f277d2a91c22ea18890a
Susan Rees <susan@reeshome.org>
2016-01-30 20:06:03 -0800

    added stringFormatting lab working file, added and finished ROT13 lab

---
+++ students/susanRees/session03/StringFormatting.py
@@ -0,0 +1,11 @@
+# Write a format string that will take:
+# ( 2, 123.4567, 10000)
+# and produce:
+# 'file_002 :   123.46, 1e+04'
+# Rewrite: "the first 3 numbers are: {:d}, {:d}, {:d}".format(1,2,3)
+# to take an arbitrary number of values
+# Trick: You can pass in a tuple of values to a function with a *:
+# code-block:: ipython
+# In [52]: t = (1,2,3)
+# In [53]: “the first 3 numbers are: {:d}, {:d}, {:d}”.format(* t)
+# Out[53]: ‘the first 3 numbers are: 1, 2, 3’
