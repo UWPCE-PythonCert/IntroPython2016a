51c8d343db1be4214b4cb0e523ece90ffe29f4bd
John Rudolph <john.rudolph08@gmail.com>
2016-01-31 20:48:18 -0500

    finished mailroom

--- students/JohnRudolph/session3/mailroom.py
+++ students/JohnRudolph/session3/mailroom.py
@@ -1,22 +1,16 @@
-'''
-################################################################
-				CREATE DONOR LIST AND ASK FOR ACTION
-################################################################
-It should have a data structure that holds a list of your donors 
-and a history of the amounts they have donated. This structure 
-should be populated at first with at least five donors, with between 
-1 and 3 donations each
 
-The script should prompt the user (you) to choose from a menu of 
-2 actions: ‘Send a Thank You’ or ‘Create a Report’
-'''
+#Mailroom assignment detail found at http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/mailroom.html#exercise-mailroom
 
-import random
+'''
+################################################################################
+						GENERATE RANDOM DONOR LIST
+################################################################################
+'''
 
 def createDonors(numDonors):
 	'''
-	This function is used to create a list of donorlist
-	Input desired number of donorlist
+	This function is used to create a donorlist
+	Input desired number of into donorlist function
 	For each donor input function will randomly generate # of donations between 1-3
 	And randomly generate a donation amount between 100-1000
 	Will return a list of lists
@@ -27,81 +21,163 @@ def createDonors(numDonors):
 	for x in range(numDonors):
 
 		numDonations = random.randint(1,3) #random # of donations between 1-3 inclusive
-		donsubList = ['donor' + str(x+1)] #list that holds donor name and donations amounts
+		donsubList = ['donor name' + str(x+1)] #list that holds donor name and donations amounts
 
-		for y in range(numDonations): #create random donations amounts
+		for y in range(numDonations): #create random donations amounts between $100-$1K
 			donsubList.append(random.randint(100,1000))
 
 		donorList.append(donsubList)
 
 	return donorList
 
-#create an arbitrary # of donors - 5 per assignment directions
-numDonors = 5
-# dreate the initial list of donors
-donorList = createDonors(numDonors)
+'''
+################################################################################
+						GENERAL FUNCTIONS
+################################################################################
+'''
 
-#create an empty dictionary - will be populated in loop below
-ownerDic = {}
-#populate donor dict from donorlist
-for donor in donorList:
-	donor_name = donor[0]
-	donations = donor[1:]
-	ownerDic[donor_name] = donations
-
-#Add an error handler so that user has to input a valid action
-index_check = None
-while index_check is None:
-	getAction = str(input("Input Action: 'Send a Thank You' or 'Create a Report\n"))
-	if getAction.lower() != ('send a thank you' or 'create a report'):
-		print("Invalid Action: Try Again!")
-	else:
-		index_check = True
+def actionPrompt():
+	'''
+	This function prompts a user to select an action: 
+	"Send thank you" or "create a report"
+	Function will loop until one of these is entered
+	'''
+	index_check = None
+	while index_check is None:
+		getAction = str(input("Input Action: 'Send a Thank You'\t'Create a Report'\t'Quit'\n"))
+		if getAction.lower() not in ['send a thank you', 'create a report', 'quit']:
+			print("Invalid Action: Try Again!")
+		else:
+			index_check = True
+			return getAction
 
 '''
 ################################################################################
-						THANK YOU ACTION
+						THANK YOU FUNCTIONS
 ################################################################################
-If the user (you) selects ‘Send a Thank You’, prompt for a Full Name.
-If the user types ‘list’, show them a list of the donor names and re-prompt
-If the user types a name not in the list, add that name to the data 
-    structure and use it.
-If the user types a name in the list, use it.
-Once a name has been selected, prompt for a donation amount.
-Verify that the amount is in fact a number, and re-prompt if it isn’t.
-Once an amount has been given, add that amount to the donation history 
-    of the selected user.
-Finally, use string formatting to compose an email thanking the donor for 
-    their generous donation. Print the email to the terminal and return to 
-    the original prompt.
 '''
-
 def appendDonor(getName):
 	'''
-	This function find the donor in the dictionary
+	This functions find the donor in the dictionary
 	Prompts user for new donations and appends donation to the dictionary key
 	If user does not enter a number then error will be raised
 	'''
 	donCheck = None
 	while donCheck is None:
 		try:
-			donation = float(input('Enter donation amount:\n'))
+			donation = int(input('Enter donation amount (int) for {}:\n'.format(getName)))
 			ownerDic.setdefault(getName, []).append(donation)
-			print(ownerDic[getName])
+			print(getName, ownerDic[getName])
 			donCheck = True
 		except:
-			print('You did not enter a number. Try Again!')
+			print('You did not enter a valid integer Try Again!')
+						
 
-if getAction == 'send a thank you':
-	getName = input("Input a full name\n")
+def sendThanks(getName):
+	'''
+	This function runs the steps for sending a thank your
+	If user select list then print current donor dic and reprompt
+	If donor is already in dic then append amounts
+	If donor not in dic then append new donor and add amount
+	'''
 	if getName.lower() == 'list':
-		print(ownerDic)
-	elif getName in ownerDic.keys():		
+		print(ownerDic.keys())
+		getName = input("Input a full name or 'list'\n")
+		sendThanks(getName)		
+	elif getName in ownerDic.keys():
+		print('Adding a new donation for: {}'.format(getName))		
 		appendDonor(getName)
+		emailThanks(getName)
+	elif getName == 'quit':
+		print('Quitting Thank You')
 	else:
-		print('Adding ', getName, ' to Donor Database')
+		print('Adding {} to Donor Database'.format(getName))
 		ownerDic.setdefault(getName, [])
 		appendDonor(getName)
+		emailThanks(getName)
+
+def emailThanks(getName):
+	'''
+	This function grabs the last donation entered for a given donor
+	Print a thank you message with donor name and donation amount
+	'''
+	donationList = list(ownerDic[getName])
+	print('Thank you {} for your generous donation of ${}!'.format(getName, donationList[-1]))
+
+'''
+################################################################################
+						DONORS REPORT FUNCTIONS
+################################################################################
+'''
+def createtopDonors():
+	'''
+	Creates a new dictionary with total donations, donation count and avg donations
+	Loops through master donations dic creates list of donations
+	Total Donations are used as new dic key
+	Returns the new dic sorted by key (total donations)
+	'''
+	sortDic = {}
+	for key in ownerDic:
+		totalDon = sum(list(ownerDic[key]))
+		countDon = len((list(ownerDic[key])))
+		avgDon = int(totalDon/countDon)
+		sortDic[totalDon] = [key, totalDon, countDon, avgDon]
+
+	return(sortDic)
+
+def outputTopDonors(topDonors):
+	'''
+	This function loops through the top donors dictionary
+	Sorts key (total donations) and prints values using tab formatting
+	'''
+	sortDonors = sorted(topDonors, reverse=True)
+	for key in sortDonors:
+		print('{}\t{}\t{}\t{}'.format(*list(topDonors[key])))	
+
+'''
+################################################################################
+							MAIN CODE BLOCK
+################################################################################
+'''
+if __name__ == '__main__':
+
+	import random
+
+	#create an arbitrary # of donors - 5 per assignment directions
+	numDonors = 5
+	# create the initial list of donors
+	donorList = createDonors(numDonors)
+	#create an empty dictionary - will be populated in loop below
+	ownerDic = {}
+	#populate donor dict from donorlist
+	for donor in donorList:
+		donor_name = donor[0]
+		donations = donor[1:]
+		ownerDic[donor_name] = donations
+
+	#flag used to quit prompting user for an action
+	quit_check = False
+
+	while quit_check == False:
+		getAction = actionPrompt()
+		if getAction.lower() == 'send a thank you':
+			getName = input("Input a full name or 'list'\n")
+			sendThanks(getName)
+		elif getAction.lower() == 'create a report':
+			topDonors = createtopDonors()
+			outputTopDonors(topDonors)
+		elif getAction.lower() == 'quit':
+			print('Quitting Program!')
+			quit_check = True
+
+	
+
+
+
+
+
+	
+
 
 
 
