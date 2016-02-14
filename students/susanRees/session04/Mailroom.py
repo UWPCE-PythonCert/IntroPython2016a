1fbc0b283ce8a4b0a36744834018fbdc2a58558e
Susan Rees <susan@reeshome.org>
2016-02-13 20:40:49 -0800

    added working menu function to mailroom lab, half finished thank function

--- students/susanRees/session04/Mailroom.py
+++ students/susanRees/session04/Mailroom.py
@@ -1,46 +1,43 @@
 # list 5 donors, amounts donated (1-3 donations each).
-a = "Ender Wiggin"
-b = "Bill Wilson"
-c = "Oliver Queen"
-d = "Buffy Summers"
-e = "Adrienne Rich"
-
-donors = [a, b, c, d, e]
-
-donorA = [100, 150, 50]
-donorB = [25, 50]
-donorC = [1000, 2500, 500]
-donorD = [25]
-donorE = [300, 50, 250]
-
-# prompt the user to ‘Send a Thank You’ or ‘Create a Report’
-
-while True:
-    selection = input("To send a thank you letter, type 1.  To create a report, type 2.")
-    if int(selection) == 1:
-        print("thank function placeholder")
-        break
-    elif int(selection) == 2:
-        print("report function placeholder")
-        break
-    else:
-        selection = input("Please type a valid selection.")
-
+donors = {"ender wiggin": [100, 150, 50], "bill wilson": [25, 50], "oliver queen": [1000, 2500, 500], "buffy summers": [25], "adrienne rich": [300, 50, 250]}
 
 # Sending a Thank You
 # user selects ‘Send a Thank You’, prompt for a Full Name.
+def thank():
+    while True:
+        fullName = input('Please input the full name of the donor you wish to thank.  To see a list of donors, type "list".')
+        fullName = str.lower(fullName)
 # user types ‘list’, show them a list of the donor names and re-prompt
+        if fullName == "list":
+            for key in sorted(donors.items()):
+                print("{}".format(key))
 # user types a name not in the list, add that name and use it.
+        elif fullName not in donors:
+            donors.update({fullName})
+            print("This is a new donor, who has now been added to the donor list.")
 # user types a name in the list, use it.
 # once name selected, prompt for a donation amount.
+        else:
 # Verify that the amount is in fact a number, if not re-prompt.
+            while True:
+                amount = input('Please input the amount of the donation.')
+                if amount != int(amount):
+                    print("Please input a valid number")
+                else:
 # Once amount given, add amount to donation history of user.
+                    donors[fullName].update({amount})
+                    break
+            break
+
 # use string formatting to compose an email thanking the donor
 # Print email to terminal and return to original prompt.
 
 # Creating a Report
 # If user selects ‘Create a Report’
+
 # Print list of donors, sorted by total donation amount.
+#            for key, value in sorted(donors.items()):
+#                print("{} : {}".format(key, value))
 # Donor Name, total donated, number donations, avg donation as value in row.
 # Using string formatting, format the output rows
 # should be tabular (values in each column should align with above and below)
@@ -76,3 +73,18 @@ while True:
 # There are many interesting decisions to make.
 # Experiment with different lengths for the lookup key.
 # (3 words, 4 words, 3 letters, etc)
+
+# prompt the user to ‘Send a Thank You’ or ‘Create a Report’
+while True:
+    selection = input('To send a thank you letter, type "thank".  To create a report, type "report".'  'To exit, type "exit".')
+    selection = str.lower(selection)
+    if selection == "thank":
+        thank()
+        break
+    elif selection == "report":
+        print("report funtion placeholder")
+        break
+    elif selection == "exit":
+        exit()
+    else:
+        print('Invalid selection.  Please type "thank" or "report".')
