07642eaea272a31b1bff2572f2a14de38052f4c3
John Rudolph <john.rudolph08@gmail.com>
2016-02-15 02:11:26 -0500

    made more updates to trap

--- students/JohnRudolph/session6/test_trap.py
+++ students/JohnRudolph/session6/test_trap.py
@@ -1,25 +1,81 @@
 #!/usr/bin/env python3
 from numpy import trapz
 from numpy import isclose
+from math import sin, cos
 from trap import trap
 
-
+#########################################################
+###############          INPUT f(x)       ###############
+#########################################################
 def linefunc(x):
     '''
     Returns f(x): y = 5
     '''
     return 5
 
-def linearfunc(x):
-    return 2 * x
 
 def linearfunc(x):
     '''
-    Returns f(x): y = 2x
+    Returns f(x): y = Ax + B
     '''
-    return 2 * x
+    return 2 * x + 5
 
-def test_line():
+
+def quadraticfunc(x):
+    ''''
+    Return f(x): y = A(x)^2 + B(x) + C
+    '''
+    return 2*x**2 + 4*x + 5
+
+
+def sinfunc(x):
+    ''''
+    Return f(x): y = sin(x)
+    '''
+    return sin(x)
+
+#########################################################
+############     TESTS USING CALC THEORY    #############
+#########################################################
+def test_linear1():
+    '''
+    Check if area under linear function for Trap
+    Is approx equal to analytical solution
+    (1/2)A(b^2 - a^2)+B(b-a)
+    '''
+    A, B, a, b = 2, 5, 0, 100
+    area1 = .5*A*(b**2 - a**2) + B*(b-a)
+    area2 = trap(linearfunc, 0, 100, 1000)
+    assert isclose(area1, area2) is True
+
+def test_quadratic1():
+    '''
+    Check if area under quadratic function for Trap
+    Is approx equal to analytical solution
+    (A/3)(b^3 - a^3) + (B/2)(b^2-a^2) + C(b-a)
+    '''
+    A, B, C, a, b = 2, 4, 5, 0, 100
+    area1 = (A/3)*(b**3 + a**3) + (B/2)*(b**2 + a**2) + C*(b-a)
+    area2 = trap(quadraticfunc, 0, 100, 1000)
+    assert isclose(area1, area2) is True
+
+
+def test_sin1():
+    '''
+    Check if area under sin function for Trap
+    Is approx equal to analytical solution
+    sin(x) = cos(a) - cos(b)
+    '''
+    a, b = 0, 100
+    area1 = cos(a) - cos(b)
+    area2 = trap(sinfunc, 0, 100, 1000)
+    assert isclose(area1, area2, atol=0.01) is True
+
+#########################################################
+###############     TESTS USING TRAPZ     ###############
+#########################################################
+
+def test_line2():
     '''
     Checks if area under curve computed for Trap func
     Is approximately equal to Trapz function from Numpy
@@ -31,15 +87,16 @@ def test_line():
         yvector.append(linefunc(i))
     area1 = trapz(yvector, x=xvector)
     x1, x2 = xvector[0], xvector[-1]
-    step = (xvector[-1]-xvector[0])/len(xvector)
+    step = len(xvector)
     area2 = trap(linefunc, x1, x2, step)
     assert isclose(area1, area2) is True
 
-def test_linear():
+
+def test_linear2():
     '''
     Checks if area under curve computed for Trap func
     Is approximately equal to Trapz function from Numpy
-    For f(x) = 2x
+    For f(x) = ax + b
     '''
     xvector = range(100)
     yvector = []
@@ -47,5 +104,40 @@ def test_linear():
         yvector.append(linearfunc(i))
     area1 = trapz(yvector, x=xvector)
     x1, x2 = xvector[0], xvector[-1]
-    step = (xvector[-1]-xvector[0])/len(xvector)
-    area2 = trap(linefunc, x1, x2, step)
+    step = len(xvector)
+    area2 = trap(linearfunc, x1, x2, step)
+    assert isclose(area1, area2) is True
+
+
+def test_quadratic2():
+    '''
+    Checks if area under curve computed for Trap func
+    Is approximately equal to Trapz function from Numpy
+    For f(x) = ax^2 + bx + c
+    '''
+    xvector = range(100)
+    yvector = []
+    for i in xvector:
+        yvector.append(quadraticfunc(i))
+    area1 = trapz(yvector, x=xvector)
+    x1, x2 = xvector[0], xvector[-1]
+    step = len(xvector)
+    area2 = trap(quadraticfunc, x1, x2, step)
+    assert isclose(area1, area2) is True
+
+
+def test_sin2():
+    '''
+    Checks if area under curve computed for Trap func
+    Is approximately equal to Trapz function from Numpy
+    For f(x) = sin(x)
+    '''
+    xvector = range(100)
+    yvector = []
+    for i in xvector:
+        yvector.append(sinfunc(i))
+    area1 = trapz(yvector, x=xvector)
+    x1, x2 = xvector[0], xvector[-1]
+    step = len(xvector)
+    area2 = trap(sin, x1, x2, step)
+    assert isclose(area1, area2, atol=0.01) is True
--- students/JohnRudolph/session6/trap.py
+++ students/JohnRudolph/session6/trap.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
 #########################################################
-##################INPUT F(X)s############################
-#########################################################
-
-def linearfunc(x):
-    return 2 * x
-
-def linefunc(x):
-    return 5
-
-#########################################################
 ###############TRAPEZOIDAL FUNCTIONS ####################
 #########################################################
+
+
+def genquadratic(x, A=1, B=1, C=1):
+    return A*x**2 + B*x + C
+
 
 def createxvector(x1, x2, step):
     '''
@@ -25,35 +20,40 @@ def createxvector(x1, x2, step):
         x1 += step
     return xvector
 
-def createyvector(fx, xvector):
+
+def createyvector(fx, xvector, **kwargs):
     '''
     Creates a vector of values based on
     a given function (fx) and given x values vector
     '''
+    keys = kwargs
     yvector = []
     for i in xvector:
-        yvector.append(fx(i))
+        yvector.append(fx(i, **keys))
     return yvector
 
-def trap(fx, x1, x2, step):
+
+def trap(fx, x1, x2, div, **kwargs):
     '''
     This function integrates under a given function
     By using the trapezoid rule
     A defined function must be supplied
     With a start value for x1 and end value x2 which is used
-    To approximate the x-axis range
+    And the number of intervals to split between x1 and x2
     '''
-    xvector = createxvector(x1, x2 + step, step)
-    yvector = createyvector(fx, xvector)
+    step = (x2 - x1) / div
+    keys = kwargs
+    xvector = createxvector(x1, x2, step)
+    yvector = createyvector(fx, xvector, **keys)
     areavector = []
-    
     cntr = -1
     for i, n in zip(xvector, yvector):
         cntr += 1
-        if 0 < cntr < len(xvector) - 1:
-            deltax = i - xvector[cntr - 1]
+        if cntr < len(xvector) - 1:
+            deltax = xvector[cntr + 1] - i
             deltay = (n + yvector[cntr + 1])/2
             areavector.append(deltax * deltay)
 
     return sum(areavector)
 
+print(trap(genquadratic, 0, 100, 20, A=2, B=2, C=2))
